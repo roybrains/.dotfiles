dc5ec3455b2ccdc662c7d0a7b7a3069565a784bd
roybrains <150239055+roybrains@users.noreply.github.com>
2024-02-13 21:41:13 +0530

    My first bash files

---
+++ first.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Today is " 'date'
+
+echo -e "\n enter the path to directory"
+read the_path
+
+echo -e "\n your path has the following: "
+ls $the_path
---
+++ for-loop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x
+set -e
+for i in {1..5}; do
+	echo "$i"
+done
